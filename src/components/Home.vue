980640d15746e1bc435a7bf8776c73a088ca2d7a
1961117443 <1961117443@qq.com>
2019-06-09 17:27:19 +0800

    初始化

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+     <h3>这是首页</h3>
+ </div>
+</template>
+<script>
+
+export default {
+    mounted(){
+       // console.log(this)
+        this.$api.get("http://localhost:3000/api/Demand/1",{},res=>{
+            console.log(res)
+        } )
+    }
+}
+</script>
+<style  scoped>
+
+</style>
+
+
+
